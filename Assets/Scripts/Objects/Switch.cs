5977d0f736f7f25e46779fc97d390e67ec9f1694
Jasmin Julia Falk <jasmin.j.falk@stud.h-da.de>
2019-06-29 11:24:19 +0200

    added switch system - if you are in the battle field you have to trigger the switch and the door opens

---
+++ Assets/Scripts/Objects/Switch.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public bool active;
+    public BoolValue storedValue;
+    //change the Sprite by Switch
+    public Sprite activeSprite;
+    //Reference to the Sprite Renderer
+    private SpriteRenderer mySprite;
+    //Reference to the Door
+    public Door thisDoor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySprite = GetComponent<SpriteRenderer>();
+        active = storedValue.RunTimeValue;
+        if (active)
+        {
+            ActivateSwitch();
+        }
+    }
+
+    public void ActivateSwitch()
+    {
+        active = true;
+        storedValue.RunTimeValue = active;
+        thisDoor.Open();
+        mySprite.sprite = activeSprite;
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        //Is it the Player?
+        if (other.CompareTag("Player"))
+        {
+            ActivateSwitch();
+        }
+    }
+}
