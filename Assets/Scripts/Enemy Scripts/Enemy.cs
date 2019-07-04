eec3d9d9850009eacab9e998a6b8a7dfa1d42059
Jasmin Julia Falk <jasmin.j.falk@stud.h-da.de>
2019-07-04 23:20:38 +0200

    added a room system, so that the enemys and other things stay by their actual place. so if an enemy follows my player and tries to hunt me until the other room but when i go back the enemy is by his home position

--- Assets/Scripts/Enemy Scripts/Enemy.cs
+++ Assets/Scripts/Enemy Scripts/Enemy.cs
@@ -22,6 +22,7 @@ public class Enemy : MonoBehaviour
     public string enemyName;
     public int baseAttack;
     public float moveSpeed;
+    public Vector2 homePosition;
 
     [Header("Death Effect")]
     public GameObject deathEffect;
@@ -30,6 +31,11 @@ public class Enemy : MonoBehaviour
     private void Awake()
     {
         health = maxHealth.initialValue;
+    }
+
+    private void OnEnable()
+    {
+        transform.position = homePosition;
     }
 
 
